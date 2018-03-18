e185355713d4667946e6a9b9f5d540d45b955969
gchangchen <gchangchen@gmail.com>
2018-03-18 16:32:03 +0800

    Initial commit

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ A simple network library for linux, use libev and libcoro.
 see async.h for api detail.It is very simple.
 
 demo.c 
-A simple demo. Include a echo server ,a echo client, and a http client.all of them are asynchronous.
+A simple demo. Include a echo server ,two echo client, and a http client.all of them are asynchronous.
--- demo.c
+++ demo.c
@@ -4,17 +4,18 @@
 #include<string.h>
 #include<unistd.h>
 
+//echo server handle.fd is the client fd.
 void echo_handle(int fd)
 {
-	char buf[1024];
-	ssize_t ret;
-
 	async_cat(fd, fd, -1, -1);
 	close(fd);
 }
 
+//echo client. arg is pass by main, not used.
 void echo_client(void *arg)
 {
+	if(arg)async_sleep(1);
+
 	int fd = async_connect_host_wto("127.0.0.1", 4000, 3, 0);
 	if(fd < 0){
 		perror("async_connect_host_wto error");
@@ -35,6 +36,7 @@ end:
 	if(fd >= 0)close(fd);
 }
 
+//http client. arg is pass by main, not used.
 void http_client(void *arg)
 {
 	async_sleep(5);
@@ -59,17 +61,27 @@ end:
 int main(int argc, char *argv[])
 {
 	if(async_init("8.8.8.8"))return -1;
+
+	//create echo server.
 	int s = create_tcp_server("127.0.0.1", 4000);
 	if(s < 0)return -1;
 	struct coroutine *co = async_accept_handle(s, echo_handle);
 	if(co == NULL)goto end;
 
+	//start a coroutine for echo client.
 	co = async_start(echo_client, NULL);
 	if(co == NULL){
 		printf("async_start echo_client error!");
 		goto end;
 	}
+	//start another coroutine for echo client.
+	co = async_start(echo_client, NULL + 2);
+	if(co == NULL){
+		printf("async_start another echo_client error!");
+		goto end;
+	}
 
+	//start a coroutine for http client.
 	co = async_start(http_client, NULL);
 	if(co == NULL){
 		printf("async_start http_client error!");
@@ -77,6 +89,7 @@ int main(int argc, char *argv[])
 	}
 
 	async_run();
+	async_destroy();
 
 end:
 	if(s>=0)close(s);
